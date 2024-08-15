84f734406353795c5e77aa6fa0d5ae8b79af52fc
sourabh kumar das <sourabhd081@gmai.com>
2024-08-15 12:10:23 +0530

    handel payment faild

--- src/components/payments/RazorpayPayment.jsx
+++ src/components/payments/RazorpayPayment.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+
 import axios from "axios";
+
 const PaymentComponent = ({ product_id }) => {
   console.log(product_id, "product id");
   const token = useSelector((state) => state.authentication.access_token);
@@ -22,11 +24,14 @@ const PaymentComponent = ({ product_id }) => {
       amount: response.data.amount,
       currency: "INR",
       order_id: response.data.order_id,
+      notes: response.data.notes,
       handler: async function (response) {
+        console.log(response);
         const paymentData = {
           razorpay_order_id: response.razorpay_order_id,
           razorpay_payment_id: response.razorpay_payment_id,
           razorpay_signature: response.razorpay_signature,
+          notes: options.notes,
         };
 
         const url_verify = "http://localhost:8000/api/payment/verify/";
@@ -35,9 +40,21 @@ const PaymentComponent = ({ product_id }) => {
         });
 
         if (verifyResponse.data.status === "success") {
+          // call a function which handels payment success
           alert("Payment successful!");
+
+          const success_data = paymentData;
+          const url_success_payment =
+            "http://localhost:8000/api/payment/success/payment/";
+          const response_success = await axios.post(
+            url_success_payment,
+            success_data,
+            { headers }
+          );
           // Handle post-payment actions (e.g., redirect, update UI)
-        } else {
+        } else if (verifyResponse.data.status === "failed") {
+          // call a function which handel failed payments
+
           alert("Payment verification failed");
         }
       },
@@ -52,6 +69,21 @@ const PaymentComponent = ({ product_id }) => {
     };
 
     const paymentObject = new window.Razorpay(options);
+
+    // paymentObject.open();
+    paymentObject.on("payment.failed", async function (response) {
+      // redirect user from here to another page with error msg
+      const res = await axios.post(
+        "http://localhost:8000/api/payment/failed/payment/",
+        response,
+        { headers }
+      );
+      console.log(response);
+      console.log(res);
+      alert("Payment failed!");
+      alert("Payment failed!");
+      alert("Payment failed!");
+    });
     paymentObject.open();
   };
 
